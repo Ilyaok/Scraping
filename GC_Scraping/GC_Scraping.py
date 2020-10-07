e459ff1de7a0d701a52c633ce3d560267be05ed2
Ilyaok <ilya.goldovskiy@gmail.com>
2020-10-08 00:32:51 +0300

    GC_Scraping

--- GC_Scraping/GC_Scraping.py
+++ GC_Scraping/GC_Scraping.py
@@ -1,44 +1,36 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from selenium import webdriver
 import pandas as pd
-import ssl
+
+driver = webdriver.Chrome()
+url = "https://ru.globalcareer.eu/candidates/"
+
+t = 1
+VacHeads = []
+Locations = []
+Dates = []
 
 
-ssl._create_default_https_context = ssl._create_unverified_context
-frame = pd.DataFrame({})
-
-vacHeads = []
-vacLocations = []
-vacDates = []
-
-def DataExtract(url):
-    html = urlopen(url)
-    bsObj = BeautifulSoup(html, features='lxml')
-
-    global vacHeads
-    global vacLocations
-    global vacDates
-
-    for vacHead in bsObj.findAll("div", {"class": "name"}):
-        vacHeads.append(vacHead.get_text())
-
-    for vacLocation in bsObj.findAll("div", {"class": "location"}):
-        if vacLocation.get_text().strip() == '':
-            vacLocations.append("N/D")
-        else:
-            vacLocations.append(vacLocation.get_text().strip())
-
-    for vacDate in bsObj.findAll("div", {"class": "job-date"}):
-        vacDates.append(vacDate.get_text())
-
-for i in range(1, 19):
-    url = "https://ru.globalcareer.eu/candidates/?PAGEN_1=" + str(i)
-    DataExtract(url)
-
-data = {"Vacancy": vacHeads, "Location": vacLocations, "Date of creation": vacDates}
-frame = pd.DataFrame(data)
+def TableCreate(ClassName, ListOfElements, driver):
+    for temp in driver.find_elements_by_class_name(ClassName):
+        ListOfElements.append(temp.text)
 
 
-writer = pd.ExcelWriter("GCVacancies.xlsx", engine='xlsxwriter')
-frame.to_excel(writer, "GCVacancies")
+while True:
+    driver.get("https://ru.globalcareer.eu/candidates/?PAGEN_1=%s" % t)
+    if t != 1 and driver.find_element_by_class_name("selected").text == "1":
+        break
+    TableCreate("name", VacHeads, driver)
+    TableCreate("location", Locations, driver)
+    TableCreate("job-date", Dates, driver)
+    t = t + 1
+
+
+
+table = {"Vacancy": VacHeads, "Location": Locations, "Date of creation": Dates}
+frame = pd.DataFrame(table)
+
+writer = pd.ExcelWriter("GC_Vacancies.xlsx", engine='xlsxwriter')
+frame.to_excel(writer, "Vacancies")
 writer.save()
+
+driver.close()
